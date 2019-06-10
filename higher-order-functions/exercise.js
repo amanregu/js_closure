700d995d6549fb34b4ea29b1a5f462682dfd9b58
Aman Regu <amanregu@gmail.com>
2019-06-10 23:51:25 +0530

    Solved challenges 1-4

--- higher-order-functions/exercise.js
+++ higher-order-functions/exercise.js
@@ -1,24 +1,41 @@
 // Challenge 1
-function addTwo(num) {}
+function addTwo(num) {
+    return num + 2
+}
 
 // To check if you've completed it, uncomment these console.logs!
 // console.log(addTwo(3));
 // console.log(addTwo(10));
 
 // Challenge 2
-function addS(word) {}
+function addS(word) {
+   return word.concat("s")
+}
 
 // uncomment these to check your work
 // console.log(addS('pizza'));
 // console.log(addS('bagel'));
 
 // Challenge 3
-function map(array, callback) {}
+function multiplyByTwo(n) {
+    return n*2;
+}
+function map(array,callback) {
+   var newArray = [];
+   for(let i = 0; i < array.length; i++) {
+       newArray.push(callback(array[i]));
+   } 	
+   return newArray;
+}
 
 // console.log(map([1, 2, 3], addTwo));
 
 // Challenge 4
-function forEach(array, callback) {}
+function forEach(array, callback) {
+    for (let i = 0; i < array.length; i++) {
+       callback(array[i])
+    }
+}
 
 // see for yourself if your forEach works!
 
